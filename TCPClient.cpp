78568eef4bf6cd2af2fd11c8a4a40f6ac0bd7afc
NhatAnh <anhhuynh09092002@gmail.com>
2023-09-08 11:31:49 +0700

    Hoàn thành bt1, inet_addr và inet_pton

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -1,4 +1,19 @@
+// BT1: CHUYỂN ĐỔI ĐỊA CHỈ IP
+// Viết 1 đoạn chương trình chuyển đổi địa chỉ IP:
+// 1. Dùng hàm inet_addr với tham số đầu vào : cp : “127.0.0.x”, với x = ([mã SV] modulo 255) + 1
+// 2. Dùng hàm inet_pton với tham số đầu vào :
+//    family : AF_INET
+//    ipstr : “127.0.0.x”, với x = ([mã SV] modulo 255) + 1
+// Xuất ra giá trị số (kiểu unsigned long) biểu diễn địa chị IP dạng nhị phân cho 2 cách trên
+
+// Huỳnh Nhật Ánh - 106200252 - 20KTMT2
+// x = (106200252 % 255) + 1 = 148
+
+
 // g++ TCPclient.cpp -o TCPclient.exe -lws2_32
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WS2tcpip.h>
+
 
 #include <Winsock2.h>
 #include <string.h>
@@ -9,6 +24,7 @@ using namespace std;
 
 int main()
 {
+    //startup
     WORD wVersion = MAKEWORD(2,2);
     WSADATA wsaDATA;
 
@@ -22,26 +38,24 @@ int main()
 
     const char*cp = "127.0.0.148";
 
+    //inet_addr
     unsigned long conv_addr;
     conv_addr = inet_addr(cp);
     cout << "By inet_addr: " << conv_addr << endl; 
 
-    const char*instr = cp;
+    //inet_pton
     int check;
-    const char*addr_out;
-    if (check = InetPtonW(AF_INET, instr, &addr_out)==-1)
+    unsigned long addr_out;
+    if ((check = inet_pton(AF_INET, cp, &addr_out)) < 1)
     {
-        cout << "Error";
+        cout << "Error Inet Pton:" << GetLastError() << endl;
     }
-    else if (check == 0)
-    {
-        cout << "Xâu không hợp lệ";
-    }
-    else 
+    else if (check == 1)
     {
         cout << "By inet_pton: "<< addr_out << endl;
     }
 
+    // Cleanup
     if((WSACleanup())==SOCKET_ERROR)    
     {
         cout << "WSACleanup failed with error" << WSAGetLastError() << endl;
